7550e05cf98b8403d438fa070aef6ed5a2b59e9e
ayundanvn15 <ayundanvn@gmail.com>
2022-12-23 08:49:27 +0700

    Install pertama

---
+++ src/main/java/com/exercise/relationshipsoneone/RelationshipsOneOneApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.relationshipsoneone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationshipsOneOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelationshipsOneOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exercise/relationshipsoneone/RelationshipsOneOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.relationshipsoneone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationshipsOneOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
